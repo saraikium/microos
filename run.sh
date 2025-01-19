5def04ad702612d70721bf31bba9af41c3e95eec
saraikium <adnank122@gmail.com>
2025-01-19 17:07:40 +0500

    Feat: add exception handling

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@ set -xue
 
 QEMU=qemu-system-riscv32
 
-# new: Path to clang and compiler flags
+# Path to clang and compiler flags
 CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
-# new: Build the kernel
+# Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     kernel.c common.c
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-    -kernel kernel.elf # new: Load the kernel
+    -kernel kernel.elf # Load the kernel
