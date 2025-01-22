b9f75bf4c12c50f9cd24d934c6219884e931a36e
saraikium <adnank122@gmail.com>
2025-01-22 17:32:06 +0500

    Fix: move READ_CSR and WRITE_CSR to common.h header file

--- kernel.h
+++ kernel.h
@@ -36,19 +36,6 @@ struct trap_frame {
   uint32_t sp;
 } __attribute__((packed));
 
-#define READ_CSR(reg)                                                          \
-  ({                                                                           \
-    unsigned long __tmp;                                                       \
-    __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                      \
-    __tmp;                                                                     \
-  })
-
-#define WRITE_CSR(reg, value)                                                  \
-  do {                                                                         \
-    uint32_t __tmp = (value);                                                  \
-    __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                    \
-  } while (0)
-
 #define PANIC(fmt, ...)                                                        \
   do {                                                                         \
     printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);      \
